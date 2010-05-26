b8593e9119a40bd920c8da5dd736b0741fcba11d
Sam Fentress <sfentress@dhcp135.concord.org>
2010-05-26 11:10:21 -0400

    New example involving tabs

--- example/apps/example/resources/main_page.js
+++ example/apps/example/resources/main_page.js
@@ -16,23 +16,46 @@ Example.mainPage = SC.Page.design({
     instructions: SC.LabelView.design(SC.StaticLayout, {
       layout: { top: 5, left: 5},
 			useStaticLayout: YES,
-			value: "Type a couple letters in the text field. Note the alerts. After two characters, view will switch to second text field. Alerts will no longer appear."
+			value: "Press the button to flip views. Tab on next view will not be selected as it should."
 		}),
     
     appContainer: SC.ContainerView.design({
       isContainerView: YES,
       layout: { top: 50, left: 50},
-      contentView: SC.TextFieldView.design({
+      contentView: SC.ButtonView.design({
         layout: {left: 20, top: 20, width: 500, height: 24},
-        isTextArea: YES,
-        valueBinding: 'Example.mainController.field1Value'
+        title: "Click me",
+        target: 'Example.mainController',
+        action: 'switchViews'
     	}),
-    	alternateView: SC.TextFieldView.create({
-    		layout: {right: 20, top: 20, width: 500, height: 24},
-        isTextArea: YES,                                    // comment out this line to make it work
-    		valueBinding: 'Example.mainController.field2Value'
-    	})
+    	
+    	alternateView: SC.View.create({
+    	  
+    	  childViews: 'tabs'.w(),
+    	  
+    	  tabs: SC.TabView.design({ 
+          layout: { left: 0, top: 100, height: 200, width: 400 },
+          items: [ 
+            {title: "Page 1", value: "Example.mainPage.page1" },
+            {title: "Page 2", value: "Example.mainPage.page2" }
+          ], 
+          itemTitleKey: 'title', 
+          itemValueKey: 'value',
+          nowShowing: 'Example.mainPage.page1' // defining the startup tab 
+        
+        })
+      })
     })
+  }),
+  
+  page1: SC.LabelView.design({
+    layout: { left: 10, top: 50 },
+    value:   'Page 1'
+  }),
+  
+  page2: SC.LabelView.design({
+    layout: { left: 300, top: 50 },
+    value: 'Page 2'
   })
 
 });
